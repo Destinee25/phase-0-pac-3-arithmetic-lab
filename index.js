9d3339d3cc85d9b656a8bdb27754600f0ac1a5f1
Destinee Robinson <Destineerobinson08@gmail.com>
2021-06-29 20:06:53 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b){
+    return a + b;
+}
+add(1,2)  
+
+function subtract(a,b){
+    return a - b;
+}
+subtract(3,2)  
+
+function multiply(a,b){
+    return a * b;
+}
+multiply(2,2)
+
+function divide(a,b){
+    return a / b;
+}
+divide(3,2)    
+
+function increment(n){
+    return n + 1;
+}
+increment(3)    
+
+function decrement(n){
+    return n - 1;
+}
+decrement(3)   
+
+function makeInt(n) {
+    return parseInt(n,10);
+}
+makeInt(n)
+
+function preserveDecimal(n){
+    return parseFloat(n,2.222);
+}
+preserveDecimal (n)
+
